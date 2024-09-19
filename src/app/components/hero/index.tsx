019e1fbfd8b50659d43cf9f32e7a1e9d729095a8
Sahil Malviya <119488054+Sahil18718@users.noreply.github.com>
2024-09-19 15:40:10 +0530

    Reverted the change (#17)
    * about and hero section
    
    * some small changes to data from one single file
    
    * Revert "some small changes to data from one single file"
    
    This reverts commit cefb001d6695ec81b77c755809c156ebf1a13df8.
    
    * Revert "Merge branch 'main' into dev"
    
    This reverts commit e4847850bbd70724a772d848f29129a26bb2ce53, reversing
    changes made to af6ed64d3ff72cd769d57a691663ee32ab77f0db.

--- src/app/components/hero/index.tsx
+++ src/app/components/hero/index.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 
-import { heroData } from './heroData'; // Adjust the path as necessary
-
 const HeroSection: React.FC = () => {
   return (
-    <section id="home" className="py-16 bg-gray-100">
-      <div className="max-w-4xl mx-auto text-center">
+    <section id="home" className="home section py-16 bg-gray-100">
+      <div className="home_container max-w-4xl mx-auto text-center">
         {/* Introduction */}
-        <h3 className="text-lg text-gray-600 mb-2" data-aos="fade-right">
-          {heroData.introduction}
+        <h3
+          className="home_container_intro text-lg text-gray-600 mb-2"
+          data-aos="fade-right"
+        >
+          Hi, my name is
         </h3>
 
         {/* Name */}
         <h1
           id="user-detail-name"
           data-aos="fade-up"
-          className="text-4xl font-bold mb-2"
+          className="home_container_intro_name text-4xl font-bold mb-2"
         >
-          {heroData.name}
+          SAHIL MALVIYA
         </h1>
 
         {/* Job Title */}
         <h1 className="text-3xl font-semibold mb-4" data-aos="zoom-in">
-          {heroData.jobTitle}
+          Full Stack Developer
         </h1>
 
         {/* Description */}
@@ -31,16 +32,19 @@ const HeroSection: React.FC = () => {
           className="text-gray-700 mb-6 max-w-xl mx-auto"
           data-aos="fade-down"
         >
-          {heroData.description}
+          Experienced generative AI professional with 2+ years of expertise,
+          eager to transition into software engineering. Proficient coder,
+          adaptable, and dedicated to learning. Track record of innovation,
+          teamwork, and exceeding expectations.
         </p>
 
         {/* Contact Button */}
         <a
-          href={heroData.contactLink}
+          href="#contact"
           className="inline-block bg-blue-500 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-600 transition-colors"
           data-aos="zoom-in-down"
         >
-          Contact Me <span className="ml-2">💬</span>
+          Contact Me <i className="fa-regular fa-message ml-2"></i>
         </a>
       </div>
     </section>
